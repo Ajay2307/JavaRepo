0f1068d66c04e79df1edb023c0ccbc6317a74fe7
Ajay2307 <47887498+Ajay2307@users.noreply.github.com>
2019-03-13 11:46:21 +0530

    Update HelloWorld.java

--- HelloWorld/src/main/java/com/kdp/java/HelloWorld.java
+++ HelloWorld/src/main/java/com/kdp/java/HelloWorld.java
@@ -1,13 +1,9 @@
-package com.kdp.java;
+public class HelloWorld 
+{
 
-public class HelloWorld {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World!!!");
+	public static void main(String[] args) 
+	{
+			System.out.println("Hello World!!!");
 	}
 
 }
